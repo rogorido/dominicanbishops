72ffc8660d2159065c3283ad6f73557385f32ccd
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2018-02-01 12:42:45 +0100

    Mejoras en shiny

--- dashb.R
+++ dashb.R
@@ -1,8 +1,6 @@
 #!/usr/bin/Rscript --vanilla
 
-library(dplyr)
-library(tidyr)
-library(shiny)
+library(tidyverse)
 library(shinydashboard)
 library(shinyWidgets) # https://github.com/dreamRs/shinyWidgets
 library(RPostgreSQL)
@@ -27,7 +25,7 @@ ui <- dashboardPage( skin = "gree",
                menuItem("Dominicos", tabName = "t_ops", icon = icon("th")),
                menuItem("Órdenes religiosas", tabName = "t_org", icon = icon("th"))),
       menuItem("Edad Moderna", tabName = "edm", icon = icon("th"),
-               menuItem("Órdenes religiosas", tabName = "t_org_edm", icon = icon("th")))
+               menuItem("Órdenes religiosas (totales)", tabName = "t_org_edm", icon = icon("th")))
   )),
 
   # body content
@@ -58,23 +56,29 @@ ui <- dashboardPage( skin = "gree",
 
         # tercero 
         tabItem(tabName = "t_org",
-                box(title="Tabla", DT::dataTableOutput("mytable1")),
-                box(title="Tabla", DT::dataTableOutput("mytable2")),
-                box(title="Tabla", DT::dataTableOutput("mytable3"))
-                ),
+                tabsetPanel(
+                    id = "tabset2",# height = "100", width="100",
+                    tabPanel("tabla1", 
+                             box(title="Tabla", DT::dataTableOutput("mytable1"))),
+                    tabPanel("tabla2", 
+                             DT::dataTableOutput("mytable2")),
+                    tabPanel("tabla3", 
+                             DT::dataTableOutput("mytable3"))
+                )),
         
         # Edad Moderna  
         tabItem(tabName = "t_org_edm",
-                selectInput("seleccionar_orden", label = h3("Seleccionar orden"), 
+                selectInput("seleccionar_orden", label = 'Probando',
                             choices = ordenes_lista,
                             selected = 'O.P.'),
-                tabBox(
-                    id = "tabset1", height = "100", width="100",
-                    tabPanel("Tab1",
-                             #box(title="Tabla", DT::dataTableOutput("mytable4"))),
-                             DT::dataTableOutput("mytable4")),
-                    tabPanel("Tab2", box(title = "Duración de obispados", width = "100%",
-                                         leafletOutput("mymap2", height = 600)))
+                tabsetPanel(
+                    id = "tabset1", #height = "100", width="100",
+                    tabPanel("Totales de obispos", DT::dataTableOutput("mytable4")),
+                    tabPanel("Duración en años", DT::dataTableOutput("mytable5")),
+                    tabPanel("Por países", DT::dataTableOutput("mytable6")),
+                    tabPanel("Mapa (total)", leafletOutput("mymap2", height = 600)),
+                    tabPanel("Mapa (duración)", leafletOutput("mymap3", height = 600)),
+                    tabPanel("Evolución", plotOutput("plotevolucion"))
                 )
                 
                 )
@@ -88,6 +92,11 @@ server <- function(input, output) {
         anos <- input$sliderduracion
         dur <- dplyr::filter(duracion, inicio >= anos[1], fin <= anos[2])
         hist(dur$duracion)
+    })
+
+    output$plotevolucion <- renderPlot({
+        em_bishops_per_year <- actualizar_em_evolucion(input$seleccionar_orden)
+        ggplot(em_bishops_per_year, aes(x=serie, y=count)) + geom_line()
     })
 
     output$mymap <- renderLeaflet({
@@ -105,6 +114,15 @@ server <- function(input, output) {
                              options = providerTileOptions(noWrap = TRUE)
                              ) %>%
             addCircleMarkers(lat = ~latitude, lng= ~longitude, radius = ~count)
+    })
+
+    output$mymap3 <- renderLeaflet({
+        em_dioc_duracion <- actualizar_em_duracion(input$seleccionar_orden)
+        leaflet(em_dioc_duracion) %>%
+            addProviderTiles(providers$Stamen.TonerLite,
+                             options = providerTileOptions(noWrap = TRUE)
+                             ) %>%
+            addCircleMarkers(lat = ~latitude, lng= ~longitude, radius = ~duracion)
     })
 
     output$mytable1 <- renderDataTable({
@@ -125,8 +143,24 @@ server <- function(input, output) {
 
     output$mytable4 <- renderDataTable({
         em_dioc <- actualizar_em_orden(input$seleccionar_orden)
-        em_dioc %>% select(diocesis, count)
+        em_dioc %>% select(diocesis, count) %>% arrange(-count)
+    })
+
+    output$mytable5 <- renderDataTable({
+        em_dioc_duracion <- actualizar_em_duracion(input$seleccionar_orden)
+        em_dioc_duracion %>% select(diocesis, duracion) %>% arrange(-duracion)
         })
+
+    output$mytable6 <- renderDataTable({
+        em_por_pais <- actualizar_em_porpais(input$seleccionar_orden)
+        em_por_pais %>% select(pais, total) %>% arrange(-total)
+        })
+
+    output$mytable7 <- renderDataTable({
+        em_por_pais_duracion <- actualizar_em_porpais_duracion(input$seleccionar_orden)
+        em_por_pais_duracion %>% select(pais, duracion) %>% arrange(-duracion)
+        })
+
 }
 
 shinyApp(ui, server)
