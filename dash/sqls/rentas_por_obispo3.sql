648feef2d503de23a81d193f31b6fbdd726acbfc
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2019-11-19 17:32:37 +0100

    Sacado dash de aquí y va a web/code

--- dash/sqls/rentas_por_obispo3.sql
+++
@@ -1,7 +0,0 @@
--- Esto está en fa9a10b6-1d48-4143-912a-05ec59ef44f4
-
-SELECT t.*
-FROM tv_rentas_por_obispo_orrg t -- esto usa la temp view anterior!
-WHERE t.order_acronym IN
-      (SELECT DISTINCT order_acronym
-       FROM b_edm_ss_sa_top10orders);
